c1ba6923d4b51976eea06264b2b694ff34a849e8
getclef <getclef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-23 02:35:18 +0000

    small style change to center button on login page
    git-svn-id: http://plugins.svn.wordpress.org/wpclef/trunk@760985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login_script.tpl.php
+++ login_script.tpl.php
@@ -2,6 +2,6 @@
     <div style="border-bottom: 1px solid #EEE; width: 90%; margin: 0 5%; z-index: 1; top: 50%; position: absolute;"></div>
     <h2 style="color: #666; margin: 0 auto 20px auto; padding: 3px 0; text-align:center; background: white; width: 20%; position:relative; z-index: 2;">or</h2>
 </div>
-<div style="margin-left: 30px;margin-bottom: 27px;">
+<div style="margin-left: 35px;margin-bottom: 27px;">
     <script src='https://clef.io/v2/clef.js' class='clef-button' data-app-id='<?php echo $app_id; ?>' data-redirect-url='<?php echo $redirect_url; ?>'></script>
 </div>
