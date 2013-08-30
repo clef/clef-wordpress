20afd21320a9b45d792a3557179dfedc7f74a30f
getclef <getclef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-30 16:05:13 +0000

    updates wordpress setup to auto fill name and URL
    git-svn-id: http://plugins.svn.wordpress.org/wpclef/trunk@764821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- keys_generation.tpl.php
+++ keys_generation.tpl.php
@@ -1,3 +1,3 @@
 <p>Getting started with Clef on your Wordpress account is easy. Use the wizard below to generate the necessary keys for your Wordpress site and you're good to go.</p>
-<iframe src="https://clef.io/iframes/wordpress?domain=<?php echo $site_domain ?>&name=<?php echo $site_name ?>" width="500" height="320"></iframe>
+<iframe src="https://clef.io/iframes/wordpress?domain=<?php echo $site_domain ?>&name=<?php echo $site_name ?>" width="500" height="350"></iframe>
 <p>Once you generate your keys, if they aren't auto-updated, copy and paste them into the fields below and click save.</p>
