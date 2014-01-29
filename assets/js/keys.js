347375eb97b1779135eed48a54e261c0cd1f55b6
Jesse Pollak <jpollak92@gmail.com>
2014-01-29 11:05:04 -0800

    removes second pop up on prompt

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,4 +1,4 @@
 /*! Clef for WordPress - v1.9
  * http://getclef.com
  * Copyright (c) 2014; * Licensed GPLv2+ */
-(function(e){function t(t){if(/http:\/\/arya.dev:5000/.test(t.origin)){var a=t.data.appID,n=t.data.appSecret,c=t.data.oauthCode,o=jQuery('input[name="wpclef[clef_settings_app_id]"]');o.val(a),e('input[name="wpclef[clef_settings_app_secret]"]').val(n),e('input[name="wpclef[clef_settings_oauth_code]"]').val(c),e(".wrap iframe").hide(),e("form#wp_clef input[type=submit]").trigger("click")}}e(document).ready(function(){window.addEventListener("message",t),setTimeout(function(){e(".logout-hook-error").slideDown()},2e4)})}).call(this,jQuery);
+(function(e){function t(t){if(/https:\/\/clef.io/.test(t.origin)){var a=t.data.appID,n=t.data.appSecret,c=t.data.oauthCode,o=jQuery('input[name="wpclef[clef_settings_app_id]"]');o.val(a),e('input[name="wpclef[clef_settings_app_secret]"]').val(n),e('input[name="wpclef[clef_settings_oauth_code]"]').val(c),e(".wrap iframe").hide(),e("form#wp_clef input[type=submit]").trigger("click")}}e(document).ready(function(){window.addEventListener("message",t),setTimeout(function(){e(".logout-hook-error").slideDown()},2e4)})}).call(this,jQuery);
