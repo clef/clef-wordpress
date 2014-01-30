bbfd50bc7cd1cffe0780aa3e36b2b7a9c8036249
Jesse Pollak <jpollak92@gmail.com>
2014-01-30 11:33:01 -0800

    fixes issue where upgrade is detected incorrectly, bumps to 1.9.1.1

--- assets/js/badge.js
+++ assets/js/badge.js
@@ -1,4 +1,4 @@
-/*! Clef for WordPress - v1.9.1
+/*! Clef for WordPress - v1.9.1.1
  * http://getclef.com
  * Copyright (c) 2014; * Licensed GPLv2+ */
 (function(e){e(document).ready(function(){var t=e(".clef-badge-prompt"),a={action:"clef_badge_prompt"},n=!1;t.find(".add-badge").click(function(i){if(i.preventDefault(),!n){n=!0;var c=e.extend({enable:"badge"},a);t.slideUp(),e.post(ajaxurl,c,function(){},"json")}}),t.find(".no-badge, .dismiss").click(function(){var n=e.extend({disable:!0},a);e.post(ajaxurl,n,function(){},"json"),t.slideUp()})})}).call(this,jQuery);
