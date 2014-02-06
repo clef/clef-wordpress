96187fffe5252b4a5cc4b46a897b77ef6fe1da2f
Mark Hudnall <hudnall.mark@gmail.com>
2014-02-05 17:59:52 -0800

    Allow users to log in with password (even if they are disabled) if they have a valid invite link: - if invite link older than 3 days, an error is shown - successful login takes them to edit profile (for now)
    Also:
    - refactor determining whether passwords are disabled for readability
    - use render_template instead of including template directly

--- includes/class.clef-login.php
+++ includes/class.clef-login.php
@@ -11,12 +11,35 @@
             add_action('login_form_login', array( __CLASS__, 'disable_login_form' ) );
             add_filter('wp_login_failed', array(__CLASS__, 'handle_login_failed'));
 
+            add_filter('login_message', array(__CLASS__, 'validate_invite'));
+
             add_filter('allow_password_reset', array(__CLASS__, 'disable_password_reset'));
+
+            add_filter('login_redirect', array(__CLASS__, 'redirect_if_invite_code'), 10, 3);
+        }
+
+        public static function redirect_if_invite_code($redirect_to, $request, $user) {
+            if (isset($_REQUEST['clef_invite_code'])) {
+                $invite_code = $_REQUEST['clef_invite_code'];
+                $invite_email = base64_decode(Clef::isset_GET('clef_invite_id'));
+                $error = self::validate_invite_code($invite_code, $invite_email);
+                if (!$error) {
+                    return get_edit_user_link();
+                }
+            }
+            return $redirect_to;
+        }
+
+        public static function validate_invite() {
+            $invite_code = Clef::isset_GET('clef_invite_code');
+            $invite_email = base64_decode(Clef::isset_GET('clef_invite_id'));
+            $error = self::validate_invite_code($invite_code, $invite_email);
+            if ($invite_code && $error) {
+                return '<div id="login_error">' . $error . '</div>';
+            }
         }
 
         public static function login_form() {
-            $app_id = self::setting( 'clef_settings_app_id' );
-            $app_secret = self::setting( 'clef_settings_app_secret' );
             if(self::is_configured()) {
                 $redirect_url = add_query_arg(array( 'clef' => 'true'), wp_login_url());
 
@@ -28,7 +51,31 @@
                     );
                 }
 
-                include CLEF_TEMPLATE_PATH."login_page.tpl.php";
+                $passwords_disabled = Clef::setting('clef_password_settings_force');
+
+                $override_key = Clef::isset_GET('override');
+                if (!self::is_valid_override_key($override_key)) {
+                    $override_key = null;
+                }
+
+                $invite_code = Clef::isset_GET('clef_invite_code');
+                $invite_email_encoded = Clef::isset_GET('clef_invite_id');
+                if (!self::has_valid_invite_code()) {
+                    $invite_code = null;
+                    $invite_email = null;
+                } else {
+                    $redirect_url = add_query_arg(array( 'invite_code' => $invite_code), $redirect_url);
+                }
+
+                $app_id = self::setting( 'clef_settings_app_id' );
+                echo Clef::render_template('login_page.tpl', array(
+                    "passwords_disabled" => $passwords_disabled,
+                    "override_key" => $override_key,
+                    "redirect_url" => $redirect_url,
+                    "invite_code" => $invite_code,
+                    "invite_email" => $invite_email_encoded,
+                    "app_id" => $app_id
+                ));
             }
         }
 
@@ -40,13 +87,65 @@
             }
         }
 
+        private static function is_valid_override_key($override_key) {
+            $valid_key = self::setting( 'clef_override_settings_key' );
+            $is_valid_override_key = 
+                (!empty($valid_key) && 
+                !empty($override_key) && 
+                $valid_key != '' &&
+                $override_key != '' &&
+                $override_key === $valid_key);
+            return $is_valid_override_key;
+        }
+
+        private static function validate_invite_code($incoming_invite_code, $email) {
+            $generic_error_message = "Sorry, that isn't a valid invite code.";
+            if (!$incoming_invite_code || !$email) {
+                return $generic_error_message;
+            }
+            $user = get_user_by('email', $email);
+            if (empty($user)) {
+                return $generic_error_message;
+            }
+            return self::validate_invite_code_for_user($incoming_invite_code, $user);
+        }
+
+        private static function validate_invite_code_for_user($incoming_invite_code, $user) {
+            $invite_code = get_user_meta($user->ID, 'clef_invite_code', true);
+            $three_days_ago = time() - 3 * 24 * 60 * 60;
+            if ((empty($invite_code)) ||
+                ($invite_code->created_at < $three_days_ago) ||
+                ($invite_code->code !== $incoming_invite_code)) {
+                    return "Sorry, this invite link has expired. " . 
+                        "Please contact your administrator for a new one.";
+            }
+        }
+
+        private static function has_valid_invite_code() {
+            if (!isset($_REQUEST['clef_invite_code']) || !isset($_REQUEST['clef_invite_id'])) {
+                return false;
+            }
+            $incoming_invite_code = $_REQUEST['clef_invite_code'];
+            $invite_email = base64_decode($_REQUEST['clef_invite_id']);
+            error_log(print_r($invite_email));
+            if (!$incoming_invite_code || !$invite_email) {
+                return false;
+            }
+            $error = self::validate_invite_code($incoming_invite_code, $invite_email);
+            return !$error;
+        }
+
         public static function disable_login_form($user) {
             if ( (self::setting( 'clef_password_settings_force' ) == 1) && empty($_POST)) {
-                $key = self::setting( 'clef_override_settings_key' );
+
+                $override_key = Clef::isset_GET('override');
+
+                $is_overridden = self::is_valid_override_key($override_key) || self::has_valid_invite_code();
+
                 if (is_user_logged_in()) {
                     header("Location: " . admin_url() );
                     exit();
-                } elseif ( !empty($key) && !empty($_GET['override']) && ($_GET['override'] === $key) ) {
+                } elseif ($is_overridden) {
                     return;
                 } else {
                     wp_enqueue_script('jquery');
@@ -63,13 +162,17 @@
         public static function disable_passwords($user) {
             if (empty($_POST)) return;
 
-            if (isset($_POST['override']) && self::valid_override($_POST['override'])) {
-                return;
+            if (isset($_POST['override']) && self::is_valid_override_key($_POST['override'])) {
+                return $user;
+            }
+            if (self::has_valid_invite_code()) {
+                return $user;
             }
 
-            $disable = self::passwords_are_disabled_for_user($user->ID);
+            $disabled_for_user = self::passwords_are_disabled_for_user($user);
+            $disabled_for_xml_rpc = !(defined('XMLRPC_REQUEST') && XMLRPC_REQUEST && self::xml_passwords_enabled());
 
-            if ($disable && !(XMLRPC_REQUEST && self::xml_passwords_enabled())) {
+            if ($disabled_for_user && $disabled_for_xml_rpc) {
                 add_filter('xmlrpc_login_error', array(__CLASS__, "return_xml_error_message"));
                 return new WP_Error('passwords_disabled', "Passwords have been disabled for this user.");
             } else {
@@ -142,7 +245,8 @@
         }
 
         public static function disable_password_reset($user_id) {
-            return !self::passwords_are_disabled_for_user($user_id);
+            $user = get_user_by('id', $user_id);
+            return !self::passwords_are_disabled_for_user($user);
         }
 
         public static function clear_logout_hook($user) {
@@ -150,11 +254,6 @@
                 unset($_SESSION['logged_in_at']);
             }
             return $user;
-        }
-
-        private static function valid_override($override) {
-            $valid_override = self::setting('clef_override_settings_key');
-            return $valid_override && $valid_override != "" && $override == $valid_override;
         }
 
         public static function return_xml_error_message() {
