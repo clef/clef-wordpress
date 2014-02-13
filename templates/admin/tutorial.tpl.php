d93297c4789ec6c3af7b27e73ce104124257cb32
Jesse Pollak <jpollak92@gmail.com>
2014-02-12 16:43:03 -0800

    adds informational slide in tutorial

--- templates/admin/tutorial.tpl.php
+++ templates/admin/tutorial.tpl.php
@@ -44,11 +44,23 @@
         <div class="button next">Continue</div>
     </div>
 </div>
+<div class="sub using-clef">
+    <div class="row">
+        <h1>Using Clef — 3 important things</h1>
+        <h3>1. Sync once, log in everywhere</h3>
+        <p>When you scan a Clef Wave, you'll be logged in to all of your sites on that computer. This means you don't have to keep scanning as you browse the web.</p>
+        <h3>2. Log out with your phone</h3>
+        <p>When you want to log out of your sites, click the logout button <b>on your phone</b>. This will log you out of all of your sites and can be done from anywhere (including after you walk away).</p>
+        <h3>3. Lose your device?</h3>
+        <p>If you lose your device, don't fret! Just visit <a href="https://getclef.com/lost">getclef.com/lost</a>, deactivate with your PIN, and reactivate on a new device.</p>
+        <div class="button button-primary button-hero next">Got it!</div>
+    </div>
+</div>
 <div class="waltz">
    <div class="row">
         <img src="<?php echo CLEF_URL . "assets/dist/img/waltz.png" ?>" alt="Waltz">
         <h1>Want to use Clef everywhere?</h1>
-        <p>Since you use Chrome, you can use Clef on sites like Facebook, Gmail, and Twitter with <a href="http://getwaltz.com">Waltz</a>.</p>
+        <p>Since you use Chrome, you can use Clef on sites like Facebook, Gmail, and Twitter with <a href="http://getwaltz.com">Waltz</a>. It takes 5 seconds to install and will give you the full Clef experience.</p>
         <a class="button button-primary button-hero" href="http://getwaltz.com" target="_blank">Get Waltz</a>
         <a href="#" class="button button-hero next">Go to settings</a>
    </div> 
