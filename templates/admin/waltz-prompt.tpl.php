e8a0c66a0e59f8115d6d4e5f94f3d7ce7a2b0384
Mark Hudnall <hudnall.mark@gmail.com>
2014-02-19 15:20:38 -0800

    open waltz in a new tab/window

--- templates/admin/waltz-prompt.tpl.php
+++ templates/admin/waltz-prompt.tpl.php
@@ -2,7 +2,7 @@
     <img src="<?php echo CLEF_URL . "assets/dist/img/waltz.png" ?>" alt="Waltz">
     <div class="text">
         <h1<?php _e(">Want to use Clef everywhere?", "clef"); ?></h1>
-        <p><?php _e("<a href='http://getwaltz.com'>Waltz</a> is a chrome extension that lets you log in to sites like Facebook, Gmail, and Twitter using Clef. It takes 5 seconds to install and will give you the full Clef experience.", "clef"); ?></p>
+        <p><?php _e("<a target='_blank' href='http://getwaltz.com'>Waltz</a> is a chrome extension that lets you log in to sites like Facebook, Gmail, and Twitter using Clef. It takes 5 seconds to install and will give you the full Clef experience.", "clef"); ?></p>
     </div>
     <div class="buttons">
         <a class="button button-primary button-hero" href="http://getwaltz.com" target="_blank"><?php _e("Try Waltz", "clef"); ?></a>
