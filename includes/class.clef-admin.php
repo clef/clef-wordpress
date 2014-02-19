9a2b9f23d7f6ff15237c6b39f65409cd2ed11198
Mark Hudnall <hudnall.mark@gmail.com>
2014-02-19 14:39:36 -0800

    Revert "plaintext email"
    This reverts commit c80a1ce6455f94cba26ba2a5772cdec9a71adf88.
    
    Conflicts:
    	includes/class.clef-admin.php

--- includes/class.clef-admin.php
+++ includes/class.clef-admin.php
@@ -167,10 +167,15 @@ class ClefAdmin {
     protected function send_invite_email($from_email, $user, $invite_code) {
         $invite_link = $invite_code->get_link();
         $to = $user->user_email;
+
         $subject = '['. get_bloginfo('name') . '] ' . __('Set up Clef for your account', "clef");
         $message = ClefUtils::render_template('invite_email.tpl', array("invite_link" =>  $invite_link), false);
         $headers = "From: WordPress <".$from_email."> \r\n";
+        add_filter('wp_mail_content_type', array('ClefUtils', 'set_html_content_type'));
+
         $sent = wp_mail($to, $subject, $message, $headers);
+
+        remove_filter('wp_mail_content_type', array('ClefUtils', 'set_html_content_type'));
         return $sent;
     }
 
--- templates/invite_email.tpl.php
+++ templates/invite_email.tpl.php
@@ -1,14 +1,4 @@
-<?php 
-$contents = __('Hi there,', 'clef') . PHP_EOL . PHP_EOL; 
-$contents .= __('The administrator for ', 'clef') . get_bloginfo('name') . ' ('. get_site_url() . ') ';
-$contents .= __('just set up Clef (https://getclef.com), which means you can now log in without passwords.', 'clef');
-$contents .= PHP_EOL . PHP_EOL;
-$contents .= __('Click this link and log in with your username and password (for the last time) to get started:', 'clef') . PHP_EOL;
-$contents .= PHP_EOL;
-$contents .= !empty($invite_link) ? $invite_link : '#';
-$contents .= PHP_EOL;
-$contents .= PHP_EOL;
-$contents .= __('Thanks!', 'clef');
-
-echo $contents;
-?>
+<p><?php _e('Hi there,', 'clef'); ?></p>
+<p><?php _e('The administrator for', 'clef') ?> <a href="<?php echo get_site_url(); ?>"><?php echo get_bloginfo('name') ?></a> <?php _e('just set up <a href="https://getclef.com">Clef</a>, which means you can now log in without passwords.', "clef"); ?></p>
+<p><?php _e('Click', 'clef'); ?> <a href="<?php if(!empty($invite_link)) { echo $invite_link; } else { echo "#"; } ?>"><?php _e('here', 'clef'); ?></a> <?php _e("and log in with your username and password (for the last time) to get started.", "clef"); ?></p>
+<p><?php _e('Thanks!', 'clef'); ?></p>
