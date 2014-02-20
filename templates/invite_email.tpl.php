a094312f0c12e2a11cdd5327e5ecb36543b587ab
Mark Hudnall <hudnall.mark@gmail.com>
2014-02-20 10:37:47 -0800

    add connect clef link to invite email preview

--- templates/invite_email.tpl.php
+++ templates/invite_email.tpl.php
@@ -1,4 +1,12 @@
 <p><?php _e('Hi there,', 'clef'); ?></p>
-<p><?php _e('The administrator for', 'clef') ?> <a href="<?php echo get_site_url(); ?>"><?php echo get_bloginfo('name') ?></a> <?php _e('just set up <a href="https://getclef.com">Clef</a>, which means you can now log in without passwords.', "clef"); ?></p>
-<p><?php _e('Click', 'clef'); ?> <a href="<?php if(!empty($invite_link)) { echo $invite_link; } else { echo "#"; } ?>"><?php _e('here', 'clef'); ?></a> <?php _e("and log in with your username and password (for the last time) to get started.", "clef"); ?></p>
+<p><?php _e('The administrator for', 'clef') ?> <?php echo get_bloginfo('name'); _e(' just set up <a href="https://getclef.com">Clef</a>, which means you can now log in without passwords.', "clef"); ?></p>
+<p><?php _e('Click', 'clef'); ?> <a href="<?php 
+if(!empty($invite_link)) { 
+    echo $invite_link; 
+} else if (isset($options) && isset($options['connectClefUrl'])) { 
+    echo $options['connectClefUrl']; 
+} else {
+    echo '#';
+}
+?>"><?php _e('here', 'clef'); ?></a> <?php _e("and log in with your username and password (for the last time) to get started.", "clef"); ?></p>
 <p><?php _e('Thanks!', 'clef'); ?></p>
