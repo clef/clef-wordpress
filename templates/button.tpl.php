acb959ced6b3bd1ce6741c3f74c251e0ff5611df
Mark Hudnall <hudnall.mark@gmail.com>
2014-02-19 17:53:56 -0800

    stop CF from mangling javascript

--- templates/button.tpl.php
+++ templates/button.tpl.php
@@ -1,7 +1,7 @@
 <?php 
 global $clef_js_included; 
 if (!$clef_js_included) { 
-    echo '<script src='.CLEF_JS_URL.'></script>';
+    echo '<script data-cfasync="false" src='.CLEF_JS_URL.'></script>';
     $clef_js_included = true;
 }
 ?>
@@ -9,7 +9,7 @@ if (!$clef_js_included) {
     data-app-id='<?php echo $app_id; ?>' 
     data-redirect-url='<?php echo $redirect_url; ?>'>
 </div>
-<script type='text/javascript'>
+<script data-cfasync="false" type='text/javascript'>
     var scripts = document.getElementsByTagName('script'),
         currentScript = scripts[scripts.length - 1],
         el = currentScript.previousElementSibling;
