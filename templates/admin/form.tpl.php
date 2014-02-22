a13264c7770082e8d64e0a73a657e03c70f6964c
Jesse Pollak <jpollak92@gmail.com>
2014-02-21 19:22:50 -0800

    readds facebook and twitter buttons

--- templates/admin/form.tpl.php
+++ templates/admin/form.tpl.php
@@ -2,7 +2,7 @@
 <div id="clef-settings-form">
     <form id="clef-form" action="options.php" method="POST" autocomplete="off">
         <h1 id="clef-settings-header"><?php _e("Clef", "clef"); ?></h1>
-        <!-- <div id="fb-root"></div>
+        <div id="fb-root"></div>
         <div class="fb-like" data-href="https://www.facebook.com/getclef" data-width="200" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
         <a href="https://twitter.com/getclef" class="twitter-follow-button" data-show-count="false" data-dnt="true">Follow @getclef</a>
         <script>(function(d, s, id) {
@@ -12,7 +12,7 @@
             js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=241719455859280";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script> -->
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         <?php settings_fields($form->id); ?>
         <div class="settings-section">
             <div class="password-settings">
