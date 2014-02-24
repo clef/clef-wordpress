4bbe76e9417efe1748b8c3ec01a3568c745d7c04
getclef <getclef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-24 21:53:49 +0000

    fixes wpmu
    git-svn-id: http://plugins.svn.wordpress.org/wpclef/trunk@864347 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class.clef-login.php
+++ includes/class.clef-login.php
@@ -42,7 +42,7 @@ class ClefLogin {
         // Allow the Clef button to be rendered anywhere
         add_action('clef_render_login_button', array($this, 'render_login_button'), 10, 2);
 
-        if (defined('WDFB_MEMBERSHIP_INSTALLED') && WDFB_MEMBERSHIP_INSTALLED) {            
+        if (defined('MEMBERSHIP_MASTER_ADMIN') && defined('MEMBERSHIP_SETACTIVATORAS_ADMIN')) {            
             add_action('signup_hidden_fields', array($this, 'add_clef_login_button_to_wpmu'));
             add_action('bp_before_account_details_fields', array($this, 'add_clef_login_button_to_wpmu'));
             add_action('membership_popover_extend_registration_form', array($this, 'add_clef_login_button_to_wpmu'));
