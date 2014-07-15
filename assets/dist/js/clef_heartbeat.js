151f4dc31b9390aa6c8778a389e26c9e2f35c868
Jesse Pollak <jpollak92@gmail.com>
2014-07-15 09:57:37 -0700

    compile new heartbeat check js

--- assets/dist/js/clef_heartbeat.js
+++ assets/dist/js/clef_heartbeat.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function() {
-  wp.heartbeat.interval("fast");
-  wp.heartbeat.enqueue("clef", "cleflogout", true);
-  return jQuery(document).on("heartbeat-tick", function(e, data) {
-    if (data && (data.cleflogout || !data["wp-auth-check"])) {
-      return window.location.reload();
-    } else {
-      return wp.heartbeat.enqueue("clef", "cleflogout", true);
-    }
-  });
+  if (wp.hearbeat) {
+    wp.heartbeat.interval("fast");
+    wp.heartbeat.enqueue("clef", "cleflogout", true);
+    return jQuery(document).on("heartbeat-tick", function(e, data) {
+      if (data && (data.cleflogout || !data["wp-auth-check"])) {
+        return window.location.reload();
+      } else {
+        return wp.heartbeat.enqueue("clef", "cleflogout", true);
+      }
+    });
+  }
 });
