bf2b2bfe199652f7e1dec98d24cc6f83c4d93aa6
Jesse Pollak <jpollak92@gmail.com>
2014-09-03 13:36:51 -0700

    updates to v2.2.4

--- clef-require.php
+++ clef-require.php
@@ -20,7 +20,7 @@ class Clef {
     }
 
     private function define_constants() {
-        define('CLEF_VERSION', '2.2.3');
+        define('CLEF_VERSION', '2.2.4');
 
         if (!defined('CLEF_IS_BASE_PLUGIN')) define('CLEF_IS_BASE_PLUGIN', false);
 
