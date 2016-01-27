4f7c34491e5e0abae57b8d5278b14097c31d47ce
jessepollak <jessepollak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-01-27 22:12:52 +0000

    2.3.4
    git-svn-id: https://plugins.svn.wordpress.org/wpclef/trunk@1337571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- clef-require.php
+++ clef-require.php
@@ -20,7 +20,7 @@ class Clef {
     }
 
     private function define_constants() {
-        define('CLEF_VERSION', '2.3.3');
+        define('CLEF_VERSION', '2.3.4');
 
         if (!defined('CLEF_IS_BASE_PLUGIN')) define('CLEF_IS_BASE_PLUGIN', false);
 
